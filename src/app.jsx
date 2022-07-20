f32af577ec7f7677e5b5f94ad5832d6b1b6590fc
modeal-namu <jangnamu@1-mm.net>
2022-07-20 18:13:55 +0900

    navbar 및 다른 input 추가를 위해 state app.jsx로 이동

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,48 @@
+import React, { Component } from 'react';
 import './app.css';
-import Habit from './components/habit';
 import Habits from './components/habits';
 
-function App() {
-  return <Habits />;
+class App extends Component {
+  state = {
+    habits : [
+        { id: 1, name : 'Reading', count: 0 },
+        { id: 2, name : 'Running', count: 0 },
+        { id: 3, name : 'coding', count: 0 },
+    ],
+  };
+
+  // + 버튼 클릭 시. 해당하는 습관의 count를 1 증가시킨다.
+  handleIncrement = habit => {
+    const habits = [...this.state.habits]; 
+    const index = habits.indexOf(habit); 
+    habits[index].count++;
+    // this.setState({ habits(state) : habits(로컬변수)});
+    this.setState({habits});
+  };
+  // - 버튼 클릭 시, 해당하는 습관의 count(1 이상일 경우)를 1 감소 시킨다.
+  handleDecrement = habit => {
+    const habits = [...this.state.habits]; 
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({habits});
+  };
+  // 삭제버튼 클릭 시, 해당하는 습관을 지운다.
+  handleDelete = habit => {
+      const habits = this.state.habits.filter(item => item.id !== habit.id);
+      this.setState({habits});
+  };
+
+  render(){
+    return (
+      <Habits 
+        habits={this.state.habits}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+        onDelete={this.handleDelete}
+      />
+    );
+  }
 }
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -6,59 +6,36 @@ import Habit from './habit';
     → 데이터 처리 능력 있음, 동작(behavior) logic, API Request, Exception Error, setState... ETC    
 */
 class Habits extends Component {
-    state = {
-        habits : [
-            { id: 1, name : 'Reading', count: 0 },
-            { id: 2, name : 'Running', count: 0 },
-            { id: 3, name : 'coding', count: 0 },
-        ],
-    };
-
     // + 버튼 클릭 시. 해당하는 습관의 count를 1 증가시킨다.
     handleIncrement = habit => {
-        const habits = this.state.habits.map(item => {
-            if(item.id === habit.id){
-                return { ...habit, count: habit.count+1 };
-            }
-            return item;
-        });
-        this.setState({habits});
-    }
+        this.props.onIncrement(habit);
+    };
     // - 버튼 클릭 시, 해당하는 습관의 count(1 이상일 경우)를 1 감소 시킨다.
     handleDecrement = habit => {
-        const habits = this.state.habits.map(item => {
-            if(item.id === habit.id){
-                if(item.count > 0){
-                    return { ...habit, count: habit.count-1 };
-                } else {
-                    return { ...habit, count: 0 };
-                }
-            }
-            return item;
-        });
-        this.setState({habits});
-    }
+        this.props.onDecrement(habit);
+    };
     // 삭제버튼 클릭 시, 해당하는 습관을 지운다.
     handleDelete = habit => {
-        const habits = this.state.habits.filter(item => {
-            return item.id != habit.id;
-        });
-        this.setState({habits});
-    }
+        this.props.onDelete(habit);
+    };
 
     render() {
-        return <ul>
-            { this.state.habits.map( habit => 
-                <Habit
-                    // props 인자 선언
-                    key={habit.id}
-                    habit={habit}
-                    onIncrement={this.handleIncrement}
-                    onDecrement={this.handleDecrement}
-                    onDelete={this.handleDelete} /> 
+        return (
+            <ul>
+                { this.props.habits.map( habit => (
+                    <Habit
+                        // props 인자 선언
+                        key={habit.id}
+                        habit={habit}
+                        onIncrement={this.handleIncrement}
+                        onDecrement={this.handleDecrement}
+                        onDelete={this.handleDelete}
+                    /> 
+                )
             )}
-        </ul>
-    }
+            </ul>
+        );
+    };
 }
 
 export default Habits;
